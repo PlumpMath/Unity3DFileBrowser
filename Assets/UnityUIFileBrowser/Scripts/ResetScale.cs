10e84fe2af1dc1bb20d60fc268f16e22f0fea292
Salvis Poišs <poisins92@gmail.com>
2016-03-25 14:18:12 +0100

    Finished (?) project

---
+++ Assets/UnityUIFileBrowser/Scripts/ResetScale.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine.UI;
+
+/// <summary>
+/// Reset object's scale
+/// </summary>
+[DisallowMultipleComponent]
+public class ResetScale : MonoBehaviour {
+
+	// Use this for initialization
+    public void Start()
+    {
+        if (gameObject.GetComponent<RectTransform>() != null)
+        {
+            gameObject.GetComponent<RectTransform>().localScale = new Vector3(1, 1, 1);
+        }
+    }
+	
+}
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# Unity3DFileBrowser
+#===========================#
+# File browser for Unity3D. #
+#===========================#
 Unity3D file browser build with uGUI
+
+Build with:
+* Unity3D v5.3.2;
+
+Scripting in C#.
